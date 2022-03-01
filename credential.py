a0b0d15ee63e386d836fc9c1ece171d83283b6af
Ayuhersi <ayub7hersi@gmail.com>
2022-03-01 12:08:49 +0300

    adding content to the functions

--- credential.py
+++ credential.py
@@ -15,12 +15,32 @@ class Credential:
         
 @classmethod
 def generate_passowrd(cls):
-    pass
+    """length of the password"""
+    
+    size = 8
+    '''generate random alpha num'''
+    alphanum = string.ascii_uppercase + string.digits + string.ascii_lowercase
+    
+    '''create password'''
+    password = ''.join(choice(alphanum) for num in range(size))
+    
+    return password 
+    
     
 @classmethod
 def display_credentials(cls, password):
-    pass
+    user_credential_list = []
+    
+    for credential in cls.credential_list:
+        if credential.user_password == password:
+            user_credential_list.append(credential)
+    return user_credential_list
 
 @classmethod
 def credential_exist(cls, name):
-    pass 
+    for credential in cls.credential_list:
+        if credential.credential_name == name:
+            return True
+    return False
+
+    
