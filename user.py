4b41a2b15161ae96c7fb54a0dce914054d8af266
Ayuhersi <ayub7hersi@gmail.com>
2022-03-01 12:15:01 +0300

    adding User class

--- user.py
+++ user.py
@@ -1,3 +1,16 @@
 from credential import Credential
 
+
+
+
+class User:
+    
+    user_list = []
+    
+    def __init__(self,user_name, user_password):
+        self.username = user_name
+        self.password = user_password
+        
+        
+
     
