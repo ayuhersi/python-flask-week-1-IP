c5009366ecafe47d5fb8564b9f79607774227380
Ayuhersi <ayub7hersi@gmail.com>
2022-03-01 10:31:39 +0300

    adding imports, adding credential list array and defining save credentials

--- credential.py
+++ credential.py
@@ -1,7 +1,17 @@
-
+from random import choice
+import string
 
 class Credential:
     
+    credential_list = []
     
     def _init_(self, user_password, credential_name, credential_password):
-        pass
+        self.user_password = user_password
+        self.credential_name = credential_name
+        self.credential_password = credential_password
+        
+    def save_credentials(self):
+        Credential.credential_list.append(self)
+        
+''''''
+        
