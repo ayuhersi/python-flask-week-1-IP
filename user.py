083376a206f9fd81e3bb6a0f1a04f725a4a11e05
Ayuhersi <ayub7hersi@gmail.com>
2022-03-01 12:42:40 +0300

    adding class methods

--- user.py
+++ user.py
@@ -11,6 +11,23 @@ class User:
         self.username = user_name
         self.password = user_password
         
-        
+    def save_user(self):
+        pass
+          
+    @classmethod 
+    def find_credential(cls, name):
+        pass 
+    
+    @classmethod 
+    def log_in(cls, name, password):
+        pass  
+    
+    @classmethod
+    def display_user(cls, name, password ):
+        pass
+      
+    @classmethod
+    def user_exist(cls, name):
+        pass 
 
     
